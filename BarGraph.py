397e7d873bcb8f587f5decefffe9264e682702a4
Gerard Marshall <gerard.marshall41@yahoo.co.uk>
2026-09-07 23:39:06 +0100

    Add volume control to LED bar graph
    This change updates the LED bar graph to read data from the Raspberry Pi over I2C, convert it into a 10-LED level, and drive the output volume through `wpctl`. It also adds a short delay between reads and tracks the volume state while keeping the LED state in sync. A local `.lgd-nfy0` artifact is ignored via `.gitignore`.

--- BarGraph.py
+++ BarGraph.py
@@ -1,22 +1,27 @@
+import subprocess
 from smbus2 import SMBus
 from gpiozero import LED
 from time import sleep
 
-
 bus = SMBus(1) # Initialize the I2C bus
-ADDR = 0x4b # Address of the Arduino
+RASP = 0x4b # Address of the Raspberry Pi
 lit = 0 # Initialize the lit variable
+volume = 0 # Initialize the volume variable
 
-PINS = [4, 27, 17, 6, 13, 19, 26, 16, 20, 21] # GPIO pins for the LEDs
+PINS = [4, 27, 17, 6, 13, 19, 26, 16, 20, 21] # GPIO pins for the LEDs 
 leds = [LED(p) for p in PINS] # Initialize LEDs
 
 while True:
-    bus.write_byte(ADDR, 0x84) # Request data from the Arduino
-    value = bus.read_byte(ADDR) # Read the data
+    bus.write_byte(RASP, 0x84) # Request data from the Raspberry Pi
+    value = bus.read_byte(RASP) # Read the data
     lit = int(value / 25.5) # Calculate the number of LEDs to light up based on the value (0-255)
-    print(value, lit) # Print the value and the number of LEDs to light up
+    print(value, lit, volume) # Print the value and the number of LEDs to light up
+    subprocess.run(["wpctl", "set-volume", "86", str(volume)]) # Set the volume using wpctl command
+    volume = value / 255
     for i in range(10): # Loop through the LEDs
         if i < lit: # If the index is less than the number of LEDs to light up
             leds[i].on() # Turn on the LED
         else: # If the index is greater than or equal to the number of LEDs to light up
             leds[i].off() # Turn off the LED
+    sleep(0.1) # Sleep for 0.1 seconds before the next iteration
+

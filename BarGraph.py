d08e70cae9d870c052699f33a896ec2836055759
Gerard Marshall <gerard.marshall41@yahoo.co.uk>
2026-09-07 21:56:20 +0100

    Map I2C value to GPIO LED bar and add gpiozero support
    Replace the simple I2C read/print loop with a GPIO-driven bar display. Initializes gpiozero LED objects for a 10-LED array (PINS list), reads a byte from the Arduino (ADDR 0x4b), scales the 0-255 value to 0-10 (lit = int(value/25.5)), and turns LEDs on/off accordingly. Adds explanatory comments and keeps the continuous polling loop. Note: depends on gpiozero and should run on a Pi with the listed GPIO pins available.

--- BarGraph.py
+++ BarGraph.py
@@ -1,11 +1,22 @@
 from smbus2 import SMBus
+from gpiozero import LED
 from time import sleep
 
-bus = SMBus(1)
-ADDR = 0x4B
+
+bus = SMBus(1) # Initialize the I2C bus
+ADDR = 0x4b # Address of the Arduino
+lit = 0 # Initialize the lit variable
+
+PINS = [4, 27, 17, 6, 13, 19, 26, 16, 20, 21] # GPIO pins for the LEDs
+leds = [LED(p) for p in PINS] # Initialize LEDs
 
 while True:
-    bus.write_byte(ADDR, 0x84)
-    value = bus.read_byte(ADDR)
-    print(value)
-    sleep(0.2)
+    bus.write_byte(ADDR, 0x84) # Request data from the Arduino
+    value = bus.read_byte(ADDR) # Read the data
+    lit = int(value / 25.5) # Calculate the number of LEDs to light up based on the value (0-255)
+    print(value, lit) # Print the value and the number of LEDs to light up
+    for i in range(10): # Loop through the LEDs
+        if i < lit: # If the index is less than the number of LEDs to light up
+            leds[i].on() # Turn on the LED
+        else: # If the index is greater than or equal to the number of LEDs to light up
+            leds[i].off() # Turn off the LED
